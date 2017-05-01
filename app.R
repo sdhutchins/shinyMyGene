76277087e26fab82d3a25e65d2865a2b95a9abe6
Shaurita Hutchins <shutchins2@umc.edu>
2017-05-01 18:10:06 -0500

    Updated gene-info-viz app.

--- app.R
+++ app.R
@@ -1,70 +1,74 @@
 library(shiny)
 library(shinythemes)
 library(shinyjs)
-library(magrittr)
-library(plyr)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(mygene)
+library(DT)
 
 getData <- function() {
   
   # read the data file
-  gData <- read.table(file.path("Data/final_gene_info.csv"), sep = ",",
-                     header = TRUE, row.names = NULL, allowEscapes = FALSE)
+  gDat <- read.table(file.path('Data/final_gene_info.csv'), sep = ",",
+                     header = TRUE, row.names = NULL, allowEscapes = FALSE,
+                     as.is = c(1,2,3,4,5,6,7))
   
-  gData
+  gDat
 }
 
 # Get the raw data
 gDataRaw <- getData()
 
 
+
 ui <- navbarPage(inverse = TRUE,
                  title = "Gene Info Viz",
-                theme = shinytheme("sandstone"),
-                
-                # Create a tab panel for the MyGene Tool
-                tabPanel("MyGene Tool",
-                         tags$footer(verbatimTextOutput("citemygene"))),
-                tabPanel("MyGene Tool"),
-  
-                # Create a tab panel for the table view
-                tabPanel("Gene Table View",
-                         fluidRow(
-                           column(4,
-                                  selectInput("Tier", "Choose Tier", choices = c("All", "1", "2", "3", "0"), selected = "1"))),
-                fluidRow(
-                  column(4,
-                         br(),
-                         downloadButton("downloadData", "Download table"),
-                         br(), br())),
-                
-                # Create a new row for the table.
-                fluidRow(
-                  DT::dataTableOutput("table1")
-  ))
+                 theme = shinytheme("sandstone"),
+                 
+                 # Create a tab panel for the MyGene Tool
+                 tabPanel("MyGene Tool",
+                          tags$footer(verbatimTextOutput("citemygene"))),
+                 tabPanel("MyGene Tool"),
+                 
+                 # Create a tab panel for the table view
+                 tabPanel("Gene Table View",
+                          fluidRow(
+                            column(4,
+                                   selectInput("t", "Gene Tier: ", c("All", unique(as.character(gDataRaw$Tier)))))),
+                          fluidRow(
+                            column(4,
+                                   br(),
+                                   downloadButton("downloadData", "Download table"),
+                                   br(), br())),
+                          
+                          # Create a new row for the table.
+                          fluidRow(
+                            DT::dataTableOutput("table1")
+                          ))
 )
 
 server <- function(input, output) {
   
-  tiers <- reactive({
-    t <- subset(gDataRaw, Tier == input$Tier)
-    return(t)
-  })
-  output$table1 <- DT::renderDataTable(DT::datatable({tiers()
+  output$table1 <- DT::renderDataTable(DT::datatable({
+    data <- gDataRaw
+    if (input$t != "All") {
+      data <- data[data$Tier == input$t,]
+    }
+    data
   },
   escape = FALSE,
   rownames = NULL,
   style = 'bootstrap'))
-
-  output$downloadData = downloadHandler(filename = function() { paste('tier', input$Tier, 'genes.csv', sep='') }, 
-                                        content = function(file) {write.csv(tiers(), file, sep = ",", row.names = FALSE)})
+  
+  # Download Button for the table
+  output$downloadData = downloadHandler(paste('tier_', input$t, '_genes.csv', sep=''), content = function(file) {
+    data <- gDataRaw
+    if (input$t != "All") {
+      data <- data[data$Tier == input$t,]
+    }
+    write.csv(data, file, sep = ",", row.names = FALSE)
+  })
   
   output$citemygene <- renderText({ invisible("Adam Mark, Ryan Thompson, Cyrus Afrasiabi and Chunlei Wu (2014). mygene: Access MyGene.Info_ services. R package version 1.10.0.") })
   
 }
 
-  
+
 shinyApp(ui = ui, server = server)
--- mygenetest.R
+++ mygenetest.R
@@ -1,14 +1,18 @@
 library(mygene)
 
+getMG <- function() {
 # Read in the data table
-gData <- read.csv(file.path("Data/final_gene_info.csv"), sep = ",", stringsAsFactors=FALSE,
+data <- read.csv(file.path("Data/final_gene_info.csv"), sep = ",", stringsAsFactors=FALSE,
                     header = TRUE, row.names = NULL, allowEscapes = FALSE)
 
 # Convert column to list
-accessions <- as.data.frame(gData$RefSeqRNA.Accession)
+accessions <- as.data.frame(data$RefSeqRNA.Accession)
 
 # Create an objectfor the queryMany results. It's a dataframe.
-results <- queryMany(accessions, scopes="accession", fields="entrezgene", species="human")
+results <- queryMany(accessions, scopes="accession", fields=c("entrezgene", "symbol", "name", "summary"), species="human")
 
+}
+
+getMG()
 
 #FYI: This is eons simpler than Python.
